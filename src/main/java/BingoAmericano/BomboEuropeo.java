b26265d9440045e9aa8a42ed9fd8d71ddec96e81
AidaMG98 <aida3montes@gmail.com>
2020-05-15 13:33:32 +0200

    Creamos todos los métodos del bomboEuropeo

--- src/main/java/BingoAmericano/BomboEuropeo.java
+++ src/main/java/BingoAmericano/BomboEuropeo.java
@@ -6,17 +6,53 @@
 package BingoAmericano;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author herma
  */
 public final class BomboEuropeo extends Bombo {
-    static final int cantidadBolas = 90;
+
+    static final int CANTIDADBOLAS = 90;
+    private static int descontar = CANTIDADBOLAS;
 
     public BomboEuropeo(List listaBombo) {
         super(listaBombo);
     }
+
+    @Override
+    public int sacarBola() {
+
+        Random alt = new Random();
+        int posicion;
+        posicion = (int) listaBombo.remove(alt.nextInt(this.descontar));
+        this.descontar--;
+        return posicion;
+    }
+
+    @Override
+    public void llenarBombo() {
+        int bolas = 90;
+
+        if (CANTIDADBOLAS == bolas) {
+            for (int i = 1; i <= bolas; i++) {
+                this.listaBombo.add(i);
+            }
+
+            System.out.println("EL BOMBO SE HA LLENO");
+        } else {
+            System.out.println("EL BOMBO YA ESTABA LLENO");
+        }
+    }
+    @Override
+     public int bolasDentro(){
+        return this.descontar;
+    }
     
+    @Override
+    public boolean vacio() {
+        return listaBombo.isEmpty();
+    }
     
 }
