5c5a02d18f06427a142ecba497453d9fb61b3de6
AidaMG98 <aida3montes@gmail.com>
2020-05-15 13:33:04 +0200

    Creamos todos los métodos de la clase BomboAmericano

--- src/main/java/BingoAmericano/BomboAmericano.java
+++ src/main/java/BingoAmericano/BomboAmericano.java
@@ -6,16 +6,54 @@
 package BingoAmericano;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author herma
  */
 public final class BomboAmericano extends Bombo {
-    static final int cantidadBolas = 75;
+
+    static final int CANTIDADBOLAS = 75;
+    private static int restar = CANTIDADBOLAS;
 
     public BomboAmericano(List listaBombo) {
         super(listaBombo);
+    }
+
+    @Override
+    public int sacarBola() {
+        
+        Random alt = new Random();
+        int posicion; 
+        posicion = (int) listaBombo.remove(alt.nextInt(this.restar));
+        this.restar--;
+        return posicion;
+    }
+
+    @Override
+    public void llenarBombo() {
+        int bolas = 75;
+        
+        if (CANTIDADBOLAS == bolas) {
+            for (int i = 1; i <= bolas; i++) {
+                this.listaBombo.add(i);
+            }
+            
+            System.out.println("EL BOMBO SE HA LLENO");
+        } else {
+            System.out.println("EL BOMBO YA ESTABA LLENO");
+        }
+    }
+    
+    @Override
+     public int bolasDentro(){
+        return this.restar;
+    }
+    
+    @Override
+      public boolean vacio(){
+          return listaBombo.isEmpty();
     }
     
 }
