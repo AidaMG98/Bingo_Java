f605482f0bb7daba426194080675165868a97d10
AidaMG98 <aida3montes@gmail.com>
2020-05-23 18:16:43 +0200

    Añadiendo funcionalidad a la calse CartonAmericano

--- src/main/java/BingoAmericano/CartonAmericano.java
+++ src/main/java/BingoAmericano/CartonAmericano.java
@@ -5,6 +5,11 @@
  */
 package BingoAmericano;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author herma
@@ -15,24 +20,51 @@ public final class CartonAmericano extends Carton {
     static public final int COLUMNAS = 5;
     Patron premio;
 
-    public CartonAmericano(Patron premio, int filas, int columnas) {
-        super(filas, columnas);
-        this.premio = premio;
+    public CartonAmericano() {
+        super(FILAS, COLUMNAS);
+        this.premio = getPatron();
     }
     
-    @Override
-    public void generarCarton(){
-    
-    }
-    
-    public Patron getPatron(){
-        return null;
-    
+    public Patron getPatron() {
+         Random aletorio = new Random();
+        int numero = aletorio.nextInt(5) + 1;
+
+        switch (numero) {
+            case 1:
+                return Patron.PATRON_C;
+            case 2:
+                return Patron.PATRON_E;
+            case 3:
+                return Patron.PATRON_F;
+            case 4:
+                return Patron.PATRON_O;
+            default:
+                return Patron.PATRON_U;
+        }
     }
 
+     @Override
+    public void generarCarton() {
+         for (int i = 0; i < super.getMatriz().length; i++) {
+             for (int j = 0; j < super.getMatriz()[i].length; j++) {
+                 super.getMatriz()[i][j]= 1;
+             }
+         }
+        
+    }
+    
     @Override
     public String toString() {
-        return "CartonAmericano{" + "premio=" + premio + '}';
+        String titulo = ("  B    I    N    G    O");
+        String carton = "\n";
+         for (int i = 0; i < this.matriz.length; i++) {
+             for (int j = 0; j < this.matriz[i].length; j++) {
+                 carton += "| " +this.matriz[i][j] + " |";
+             }
+             carton += "\n";
+        }
+        
+        return titulo + carton;
     }
-
+    
 }
