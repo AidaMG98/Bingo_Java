a13c87e5408fe260ffd309cbe6d437eb77284d2c
AidaMG98 <aida3montes@gmail.com>
2020-05-30 12:14:14 +0200

    Creamos los métodos de la clase IBingo

--- src/main/java/BD/IBingo.java
+++ src/main/java/BD/IBingo.java
@@ -5,10 +5,28 @@
  */
 package BD;
 
+import BingoAmericano.Bingo;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author herma
  */
 public interface IBingo {
     
+    // Método para obtener todos los registros de la tabla
+    List<Bingo> mostrarDatos() throws SQLException;
+
+    // Método para generar partida guardada
+    Bingo cargarPartida(String pk) throws SQLException;
+
+    // Método para insertar una nueva partida de bingo
+    int partidaNueva(Bingo nuevo) throws SQLException;
+
+    // Método para borrar una partida del bingo
+    int borrarPartida(Bingo borrar) throws SQLException;
+
+    // Método para modificar el nombre de la persona
+    int actualizarNombre(String pk, Bingo actualizar) throws SQLException;
 }
