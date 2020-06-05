ebd027c43e6572031f4006ebb49863bace0f1b03
AidaMG98 <aida3montes@gmail.com>
2020-06-05 15:01:13 +0200

    Documentamos la clase Bombo

--- src/main/java/BingoAmericano/Bombo.java
+++ src/main/java/BingoAmericano/Bombo.java
@@ -13,32 +13,43 @@ import java.util.List;
  * @author herma
  */
 public abstract class Bombo {
+
+    /*Atributo*/
     private final List<Integer> listaBombo;
 
+    /*Constructor*/
     public Bombo() {
         this.listaBombo = new ArrayList<>();
     }
-    
-    public int sacarBola(){
+
+    /*Método de sacarBola (Saca la primera bola y la elimina)*/
+    public int sacarBola() {
         return listaBombo.remove(0);
     }
-    
+
+    /*Método de LlenarBombo (Se mete la lista dentro de un for con la cantidad de bolas
+    de cada tipo de bombo y se va añadiendo valores a la lista de forma ordenada. 
+    Luego se desordena)*/
     public abstract void llenarBombo();
-    
-    public int bolasDentro(){
+
+    /*Método de bolasDentro (Indica el tamaño de lista)*/
+    public int bolasDentro() {
         return listaBombo.size();
     }
-    
-    public boolean vacio(){
+
+    /*Método que comprueba si esta vacio el bombo*/
+    public boolean vacio() {
         return listaBombo.isEmpty();
     }
 
+    /*Getter*/
     public List<Integer> getListaBombo() {
         return listaBombo;
     }
 
+    /*toString*/
     @Override
     public String toString() {
-        return  " "+listaBombo;
+        return " " + listaBombo;
     }
 }
